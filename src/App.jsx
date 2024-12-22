393526ab47b92d16309f3d451aaad0b7ce26ca1c
BenjaminVeli <benjamin.veli@tecsup.edu.pe>
2024-12-21 23:11:41 -0500

    ERROR

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,14 @@
-import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-
-import Hero from "./sections/Hero";
-import Involved from "./sections/Involved";
-import Features from "./sections/Features";
-import Price from "./sections/Pricing";
-import Expectation from "./sections/Expectation";
-import CallToAction from "./sections/CallToAction";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Start from "./pages/Start.jsx";
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const preloader = document.getElementById("preloader");
-
-    const timer = setTimeout(() => {
-      if (preloader) {
-        preloader.classList.add("fade-out");
-        setLoading(false); // Cambia el estado a false después del timeout
-      }
-    }, 3000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
-    <Router>
-      <div id="preloader">
-        <div className="loader">
-          <span className="loader--blue">Tomso ai</span>
-        </div>
-      </div>
-
-      {!loading && (
-        <>
-          <Header />
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <>
-                  <Hero />
-                  <main>
-                    <Involved />
-                    <Features />
-                    <Price />
-                    <Expectation />
-                    <CallToAction />
-                  </main>
-                </>
-              }
-            />
-            {/* Puedes agregar más rutas aquí si es necesario */}
-          </Routes>
-          <Footer />
-        </>
-      )}
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Start />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Start.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+import Hero from "../sections/Hero";
+import Involved from "../sections/Involved";
+import Features from "../sections/Features";
+import Price from "../sections/Pricing";
+import Expectation from "../sections/Expectation";
+import CallToAction from "../sections/CallToAction";
+
+const Start = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const preloader = document.getElementById("preloader");
+
+    const timer = setTimeout(() => {
+      if (preloader) {
+        preloader.classList.add("fade-out");
+        setLoading(false); // Cambia el estado a false después del timeout
+      }
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+  return (
+    <>
+      <div id="preloader">
+        <div className="loader">
+          <span className="loader--blue">Tomso ai</span>
+        </div>
+      </div>
+      {!loading && (
+        <>
+          <Header />
+          <Hero />
+          <main>
+            <Involved />
+            <Features />
+            <Price />
+            <Expectation />
+            <CallToAction />
+          </main>
+          <Footer />
+        </>
+      )}
+    </>
+  );
+};
+
+export default Start;
